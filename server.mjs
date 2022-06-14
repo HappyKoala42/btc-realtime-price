bfda04ad49d126b206579c8034795c98a20a0402
SolidLinks <aryan.motiondash@gmail.com>
2022-06-13 23:00:08 -0400

    Graph is drawn properly, Added clock and prepData function to server.mjs

--- server.mjs
+++ server.mjs
@@ -27,56 +27,107 @@ app.use(bp.urlencoded({ extended: true }));
 
 let url = "https://api.binance.com/api/v1/ticker/24hr"
 let currency = 'BTCUSDT'
-let memory;
-let prices = [];
+var memory = 25000;
+var prices = [];
+var clocks = [];
+var dates = [];
 
-async function scrapePrice() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    
-    const [element] = await page.$x('/html/body/pre');
-    const context = await element.getProperties('textContent');
-    let string = await context.jsonValue();
-    
-    
-    // close the browser
-    await browser.close();
-    
-    
-    // eliminate other currencies
-    string = string.substring(string.indexOf(currency));
-    string = string.split("symbol")[0];
-    string = string.replace(/"|,|:|{|}/g,'');
-    string = string.substring(string.indexOf("lastPrice")+9);
-    string = string.split("lastQty")[0];
-    let price = parseFloat(string);
+var outPrices = [];
+var outClocks = [];
+var outDates = [];
 
-    if (prices.length > memory) {
-        prices.shift();
-    }
-
-    await Promise.reject(new Error('scrapePrice'));
+async function getDate()
+{
+    var today = new Date().toLocaleDateString("en-GB", {
+        hour12: false,
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+    today = today.replace(/,/g,'');
+    return today;
 }
 
-(function() {
-    app.post("/", (req, res) => {
-        console.log("Pinged");
 
+async function scrapePrice()
+{
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const [element] = await page.$x('/html/body/pre');
+        const context = await element.getProperty('textContent');
+        var string = await context.jsonValue();
+        await browser.close(); 
+
+        string = string.substring(string.indexOf(currency));
+        string = string.split("symbol")[0];
+        string = string.replace(/"|,|:|{|}/g,'');
+        string = string.substring(string.indexOf("lastPrice")+9);
+        string = string.split("lastQty")[0];
+        prices.push(parseFloat(string)); 
+        clocks.push(Math.round((new Date().getTime())/1000));
+        dates.push(await getDate()); 
+
+        if(prices.length > memory)
+        {
+            prices.shift();
+            clocks.shift();
+            dates.shift();
+        }
+
+        await Promise.reject(new Error('scrapePrice'));
+}
+
+function prepData(interval)
+{
+    outPrices = [];
+    outClocks = [];
+    outDates = [];
+
+    var copyPrices = prices;
+    var copyClocks = clocks;
+    var copyDates = dates;
+
+    var intervalTime = Math.round((new Date().getTime())/1000) - interval;
+
+    for(let i = copyPrices.length-1; i > -1; i--)
+    {
+        if(copyClocks[i] > intervalTime)
+        {
+            outPrices.unshift(copyPrices[i]);
+            outClocks.unshift(copyClocks[i]);
+            outDates.unshift(copyDates[i]);
+        }
+        else
+            i = -1;
+    }
+}
+
+(function()
+{ 
+    app.post("/", (req, res) => 
+    {
+        prepData(req.body.bufferTime);
+        console.log(Math.round((new Date().getTime())/1000)+' - got pinged');
         res.json({
             currency: currency,
-            prices: prices
+            prices: outPrices,
+            clocks: outClocks,
+            dates: outDates
         }),
-
         res.end();
     })
-    app.listen(port, () => {console.clear(), console.log("Listening to port 3000")});
-}());
+    app.listen(port, () => {console.clear(),console.log('Tuning on port 3000','\n--------------------')})
+})();
 
-(async function loop() {
-    setTimeout(async function() {
+(async function loop() 
+{
+    setTimeout(async function () 
+    {   
         await scrapePrice().catch(() => {});
         await loop();
-        // console.log(prices);
     }, 1000);
-}());
+}()); 
